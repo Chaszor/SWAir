b84964603d1d13b2cda6bda062dc73c61358fbd3
Curt Bennett <chaszor@gmail.com>
2024-02-07 01:01:58 -0600

    Latest Update
    added signup.html, signup.php, and logindb.sql. modified home.html and login.html to reflect new signup.html.
    logindb.sql reflects username and password currently and has an account number as UPK.

---
+++ logindb.sql
@@ -0,0 +1,49 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1:3306
+-- Generation Time: Feb 07, 2024 at 06:57 AM
+-- Server version: 8.2.0
+-- PHP Version: 8.2.13
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `logindb`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+DROP TABLE IF EXISTS `users`;
+CREATE TABLE IF NOT EXISTS `users` (
+  `AccountNumber` int NOT NULL AUTO_INCREMENT,
+  `Username` varchar(25) NOT NULL,
+  `Password` int NOT NULL,
+  PRIMARY KEY (`AccountNumber`),
+  UNIQUE KEY `AccountNumber` (`AccountNumber`)
+) ENGINE=MyISAM AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`AccountNumber`, `Username`, `Password`) VALUES
+(1, 'Test1', 1234);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
---
+++ signup.php
@@ -0,0 +1,20 @@
+<?php
+error_reporting(0);
+
+$Username=$_POST['username'];
+$Password=$_POST['password'];
+$con = mysql_connect("localhost", "root", "");
+
+if(!$con){
+    die('could not connect:'.mysql_error());
+}
+mysql_select_db("logindb", $con);
+
+$query="INSERT INTO users(Username,Password)VALUES('$Username', '$Password')";
+if(!mysql_query($query,$con)){
+    die('Error in inserting records' .mysql_error);
+    }else
+    {
+        echo "Account Created";
+    }
+?>
