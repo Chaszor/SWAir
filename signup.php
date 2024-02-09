4188d1398c3a84874ca460fafc976255b4ee6ed3
Curt Bennett <chaszor@gmail.com>
2024-02-08 21:16:16 -0600

    update with changes after the sprint

--- signup.php
+++ signup.php
@@ -1,8 +1,9 @@
 <?php
 error_reporting(0);
 
-$Username=$_POST['username'];
+$email=$_POST['email'];
 $Password=$_POST['password'];
+$name=$_POST['name'];
 $con = mysql_connect("localhost", "root", "");
 
 if(!$con){
@@ -10,7 +11,7 @@ if(!$con){
 }
 mysql_select_db("logindb", $con);
 
-$query="INSERT INTO users(null,Username,Password) VALUES('$Username', '$Password')";
+$query="INSERT INTO users(Email,Password,Name) VALUES('$email', '$Password', '$name')";
 if(!mysql_query($query,$con)){
     die('Error in inserting records' .mysql_error);
     }else
@@ -18,3 +19,5 @@ if(!mysql_query($query,$con)){
         echo "Account Created";
     }
 ?>
+
+//INSERT INTO `users` (`AccountNumber`, `AccountType`, `Email`, `Password`, `Name`, `FlightDate1`, `FlightDate2`, `FlightDate3`, `SeatNumber1`, `SeatNumber2`, `SeatNumber3`, `CheckedBag1`, `CheckedBag2`, `CheckedBag3`) VALUES (NULL, '1', 'test2@gmail.com', '9876', 'Test Employee 2', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL);
